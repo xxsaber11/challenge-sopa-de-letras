f141899a400caeacacf2f262031bb075279dd5c9
sebastian godirio <sebastianvagodirioo@yahoo.com.ar>
2024-02-19 16:19:47 -0300

    FIX/ se arreglo Tests

--- src/test/java/com/challenge/soup/sorting/SortingTest.java
+++ src/test/java/com/challenge/soup/sorting/SortingTest.java
@@ -1,17 +1,25 @@
 package com.challenge.soup.sorting;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.challenge.soup.models.jugadores.JugadorModel;
+import com.challenge.soup.servicesImpl.SortingServiceImpl;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SortingTest {
 
+    @Autowired
+    private SortingServiceImpl sorting;
     @Test
     public void sort1Completo() {
         JugadorModel p1 = new JugadorModel("Matias", 100);
@@ -19,61 +27,81 @@ public class SortingTest {
         JugadorModel p3 = new JugadorModel("Enzo", 50);
         JugadorModel p4 = new JugadorModel("Junior", 75);
         JugadorModel p5 = new JugadorModel("Pablo", 150);
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModeles.add(p1);
-        JugadorModeles.add(p2);
-        JugadorModeles.add(p3);
-        JugadorModeles.add(p4);
-        JugadorModeles.add(p5);
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();
+        Jugadores.add(p1);
+        Jugadores.add(p2);
+        Jugadores.add(p3);
+        Jugadores.add(p4);
+        Jugadores.add(p5);
 
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionYNombre(JugadorModeles);
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
+
 
         JugadorModel p1Sort = new JugadorModel("Pablo", 150);
         JugadorModel p2Sort = new JugadorModel("Alejandro", 100);
         JugadorModel p3Sort = new JugadorModel("Matias", 100);
         JugadorModel p4Sort = new JugadorModel("Junior", 75);
         JugadorModel p5Sort = new JugadorModel("Enzo", 50);
-        List<JugadorModel> JugadorModelesSort = new ArrayList<JugadorModel>();
-        JugadorModelesSort.add(p1Sort);
-        JugadorModelesSort.add(p2Sort);
-        JugadorModelesSort.add(p3Sort);
-        JugadorModelesSort.add(p4Sort);
-        JugadorModelesSort.add(p5Sort);
+        List<JugadorModel> JugadoresSort = new ArrayList<JugadorModel>();
+        JugadoresSort.add(p1Sort);
+        JugadoresSort.add(p2Sort);
+        JugadoresSort.add(p3Sort);
+        JugadoresSort.add(p4Sort);
+        JugadoresSort.add(p5Sort);
 
 
-        assertEquals(JugadorModelesSort.size(), JugadorModelesResult.size());
-        assertEquals(JugadorModelesSort.get(0).getNombre(), JugadorModelesResult.get(0).getNombre());
-        assertEquals(JugadorModelesSort.get(1).getNombre(), JugadorModelesResult.get(1).getNombre());
-        assertEquals(JugadorModelesSort.get(2).getNombre(), JugadorModelesResult.get(2).getNombre());
-        assertEquals(JugadorModelesSort.get(3).getNombre(), JugadorModelesResult.get(3).getNombre());
-        assertEquals(JugadorModelesSort.get(4).getNombre(), JugadorModelesResult.get(4).getNombre());
+        assertEquals(JugadoresSort.size(), JugadoresResult.size());
+        assertEquals(JugadoresSort.get(0).getNombre(), JugadoresResult.get(0).getNombre());
+        assertEquals(JugadoresSort.get(1).getNombre(), JugadoresResult.get(1).getNombre());
+        assertEquals(JugadoresSort.get(2).getNombre(), JugadoresResult.get(2).getNombre());
+        assertEquals(JugadoresSort.get(3).getNombre(), JugadoresResult.get(3).getNombre());
+        assertEquals(JugadoresSort.get(4).getNombre(), JugadoresResult.get(4).getNombre());
 
-        JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModelesResult = Sorting.ordenarPorPuntuacionYNombre(JugadorModeles);
-        assertTrue(JugadorModelesResult.isEmpty());
+        Jugadores = new ArrayList<JugadorModel>();
+        Map<String, Object> resultado2 = new HashMap<>();
+        resultado2 = sorting.ordenarPorPuntuacionYNombre(Jugadores);
+        JugadoresResult = objectMapper.convertValue(resultado2.get("response"), new TypeReference<List<JugadorModel>>() {});
+        assertTrue(JugadoresResult.isEmpty());
 
-        JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModeles.add(p1);
-        JugadorModelesResult = Sorting.ordenarPorPuntuacionYNombre(JugadorModeles);
-        assertEquals(1, JugadorModelesResult.size());
-        assertEquals(p1.getNombre(), JugadorModelesResult.get(0).getNombre());
+        Jugadores = new ArrayList<JugadorModel>();
+        Jugadores.add(p1);
+
+        Map<String, Object> resultado3 = new HashMap<>();
+        resultado3 = sorting.ordenarPorPuntuacionYNombre(Jugadores);
+        JugadoresResult = objectMapper.convertValue(resultado3.get("response"), new TypeReference<List<JugadorModel>>() {});
+
+        assertEquals(1, JugadoresResult.size());
+        assertEquals(p1.getNombre(), JugadoresResult.get(0).getNombre());
     }
 
     @Test
     public void sort1Vacio() {
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionYNombre(JugadorModeles);
-        assertTrue(JugadorModelesResult.isEmpty());
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();
+
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
+
+        assertTrue(JugadoresResult.isEmpty());
     }
 
     @Test
     public void sort1Unico() {
         JugadorModel p1 = new JugadorModel("Matias", 100);
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModeles.add(p1);
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionYNombre(JugadorModeles);
-        assertEquals(1, JugadorModelesResult.size());
-        assertEquals(p1.getNombre(), JugadorModelesResult.get(0).getNombre());
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();
+        Jugadores.add(p1);        
+        
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
+
+        assertEquals(1, JugadoresResult.size());
+        assertEquals(p1.getNombre(), JugadoresResult.get(0).getNombre());
     }
 
     @Test
@@ -84,14 +112,17 @@ public class SortingTest {
         JugadorModel p3 = new JugadorModel("Enzo", 75, 5);
         JugadorModel p4 = new JugadorModel("Junior", 75, 30);
         JugadorModel p5 = new JugadorModel("Pablo", 150, 0);
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModeles.add(p1);
-        JugadorModeles.add(p2);
-        JugadorModeles.add(p3);
-        JugadorModeles.add(p4);
-        JugadorModeles.add(p5);
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();
+        Jugadores.add(p1);
+        Jugadores.add(p2);
+        Jugadores.add(p3);
+        Jugadores.add(p4);
+        Jugadores.add(p5);
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionPerdidasYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
 
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionPerdidasYNombre(JugadorModeles);
 
         JugadorModel p1Sort = new JugadorModel("Pablo", 150, 0);
         JugadorModel p2Sort = new JugadorModel("Matias", 100, 3);
@@ -99,37 +130,46 @@ public class SortingTest {
         JugadorModel p4Sort = new JugadorModel("Enzo", 75, 5);
         JugadorModel p5Sort = new JugadorModel("Junior", 75, 30);
 
-        List<JugadorModel> JugadorModelesSort = new ArrayList<JugadorModel>();
-        JugadorModelesSort.add(p1Sort);
-        JugadorModelesSort.add(p2Sort);
-        JugadorModelesSort.add(p3Sort);
-        JugadorModelesSort.add(p4Sort);
-        JugadorModelesSort.add(p5Sort);
+        List<JugadorModel> JugadoresSort = new ArrayList<JugadorModel>();
+        JugadoresSort.add(p1Sort);
+        JugadoresSort.add(p2Sort);
+        JugadoresSort.add(p3Sort);
+        JugadoresSort.add(p4Sort);
+        JugadoresSort.add(p5Sort);
 
 
-        assertEquals(JugadorModelesSort.size(), JugadorModelesResult.size());
-        assertEquals(JugadorModelesSort.get(0).getNombre(), JugadorModelesResult.get(0).getNombre());
-        assertEquals(JugadorModelesSort.get(1).getNombre(), JugadorModelesResult.get(1).getNombre());
-        assertEquals(JugadorModelesSort.get(2).getNombre(), JugadorModelesResult.get(2).getNombre());
-        assertEquals(JugadorModelesSort.get(3).getNombre(), JugadorModelesResult.get(3).getNombre());
-        assertEquals(JugadorModelesSort.get(4).getNombre(), JugadorModelesResult.get(4).getNombre());
+        assertEquals(JugadoresSort.size(), JugadoresResult.size());
+        assertEquals(JugadoresSort.get(0).getNombre(), JugadoresResult.get(0).getNombre());
+        assertEquals(JugadoresSort.get(1).getNombre(), JugadoresResult.get(1).getNombre());
+        assertEquals(JugadoresSort.get(2).getNombre(), JugadoresResult.get(2).getNombre());
+        assertEquals(JugadoresSort.get(3).getNombre(), JugadoresResult.get(3).getNombre());
+        assertEquals(JugadoresSort.get(4).getNombre(), JugadoresResult.get(4).getNombre());
     }
 
     @Test
     public void sort2Vacio() {
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionPerdidasYNombre(JugadorModeles);
-        assertTrue(JugadorModelesResult.isEmpty());
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();        
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionPerdidasYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
+
+        assertTrue(JugadoresResult.isEmpty());
     }
 
     @Test
     public void sort2Unico() {
         JugadorModel p1 = new JugadorModel("Matias", 100);
-        List<JugadorModel> JugadorModeles = new ArrayList<JugadorModel>();
-        JugadorModeles.add(p1);
-        List<JugadorModel> JugadorModelesResult = Sorting.ordenarPorPuntuacionPerdidasYNombre(JugadorModeles);
-        assertEquals(1, JugadorModelesResult.size());
-        assertEquals(p1.getNombre(), JugadorModelesResult.get(0).getNombre());
+        List<JugadorModel> Jugadores = new ArrayList<JugadorModel>();
+        Jugadores.add(p1);
+        
+        Map<String, Object> resultado = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        resultado = sorting.ordenarPorPuntuacionPerdidasYNombre(Jugadores);
+        List<JugadorModel> JugadoresResult = objectMapper.convertValue(resultado.get("response"), new TypeReference<List<JugadorModel>>() {});
+
+        assertEquals(1, JugadoresResult.size());
+        assertEquals(p1.getNombre(), JugadoresResult.get(0).getNombre());
     }
 
 
